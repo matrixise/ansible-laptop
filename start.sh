5ef20d4020610eceee37511c6863e49b5dd56824
Stéphane Wirtel <stephane@wirtel.be>
2020-02-20 23:40:27 +0100

    Comment

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script is just used for the bootstrap, it will download the definitive
+# script and will apply the ansible playbook
 sudo apt install -y curl
 
 curl -sSL https://raw.githubusercontent.com/matrixise/ansible-laptop/master/bootstrap.sh | bash
